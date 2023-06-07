219787407f01d5ccbff2c82927d3f02a5188af92
Anthony GODART <107860091+AnthonyGodart@users.noreply.github.com>
2023-06-07 13:54:39 +0200

    improved responsive and style

--- kasa/src/index.jsx
+++ kasa/src/index.jsx
@@ -7,19 +7,22 @@ import Home from './pages/Home'
 import Housing from './pages/Housing'
 import Footer from './components/Footer'
 import Header from './components/Header'
+import styles from '../src/style.module.css'
 
 const container = document.getElementById('app');
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
 root.render(
   <React.StrictMode>
     <Router>
-      <Header />
+      <div className={styles.container}>
+        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/About" element={<About />} />
           <Route path="/Housing/:id" element={<Housing />} />
           <Route path="/Error404" element={<Error404 />} />
         </Routes>
+      </div>
       <Footer />    
     </Router>
   </React.StrictMode>
