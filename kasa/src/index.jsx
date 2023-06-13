c9f951a4edf0a82a6a8684fc2f3618d4e08733be
Anthony GODART <107860091+AnthonyGodart@users.noreply.github.com>
2023-06-13 13:32:18 +0200

    detailed footer CSS

--- kasa/src/index.jsx
+++ kasa/src/index.jsx
@@ -1,16 +1,17 @@
-import React from 'react'
-import {createRoot} from 'react-dom/client'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import About from './pages/About'
-import Error404 from './pages/Error404'
-import Home from './pages/Home'
-import Housing from './pages/Housing'
-import Footer from './components/Footer'
-import Header from './components/Header'
-import styles from '../src/style.module.css'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import About from './pages/About';
+import Error404 from './pages/Error404';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Home from './pages/Home';
+import Housing from './pages/Housing';
+import styles from '../src/style.module.css';
 
 const container = document.getElementById('app');
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
+
 root.render(
   <React.StrictMode>
     <Router>
@@ -26,4 +27,4 @@ root.render(
       <Footer />    
     </Router>
   </React.StrictMode>
-)
+);
